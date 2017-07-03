0366a6d31e8ee7942045854dd89f7744b664797f
XiaoYu-CN <xiaojinghang@foxmail.com>
2017-07-03 23:12:22 +0800

    change permission

--- createuser.sh
+++ createuser.sh
@@ -1,11 +1,10 @@
 useradd oracle
-mkdir ~oracle/.ssh
+mkdir -m 700 ~oracle/.ssh
 # get public key from instance metadata and store it in authorized_keys file
 PUBKEYURL=http://192.0.0.192/latest/meta-data/public-keys/0/openssh-key
 curl --silent -o ~oracle/.ssh/authorized_keys $PUBKEYURL
-chown $(id -un oracle):$(id -gn oracle) ~oracle/.ssh/authorized_keys
+chown -R $(id -un oracle):$(id -gn oracle) ~oracle/.ssh
 # directory & file permission is strictly set, otherwise SSH is not allowed
-chmod 700 ~oracle/.ssh
 chmod 600 ~oracle/.ssh/authorized_keys
 # allow user sudo access
 usermod -G ADMINS oracle
