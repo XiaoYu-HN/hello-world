6f1c23fcc4d98aaad12d5f4cd21c1284af3b6593
XiaoYu-CN <xiaojinghang@foxmail.com>
2017-07-03 22:09:41 +0800

    add pubkey

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,6 @@
 useradd oracle
-hostname --fqdn | tee -a /tmp/bootstrap.log
-id | tee -a /tmp/bootstrap.log
+PUBKEYURL=http://192.0.0.192/latest/meta-data/public-keys/0/openssh-key
+curl --silent -o ~oracle/.ssh/authorized_keys $PUBKEYURL
+chown $(id -un oracle):$(id -gn oracle) ~oracle/.ssh/authorized_keys
+chmod 700 ~oracle/.ssh
+chmod 600 ~oracle/.ssh/authorized_keys
